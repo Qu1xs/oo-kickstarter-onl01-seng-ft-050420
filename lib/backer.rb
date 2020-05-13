7cb34eb1fbddf2492b14f530f532ac830da069f8
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-13 20:04:02 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,12 +3,9 @@ class Backer
   attr_reader :name, :title
   attr_writer
   
-  @backend_projects = []
-  
   def initialize(name)
     @name = name
-    @backend_projects
-    @backers = []
+    @backend_projects = []
   end
   
   
